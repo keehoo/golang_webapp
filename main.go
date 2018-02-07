3f801635d2054f826ee9a8fe8b0fb342b6614d67
Krzysztof Kubicki <krzysztof.kubicki@fancyfon.com>
2018-02-07 17:37:18 +0100

    Tests the webserver, handler, router etc

--- main.go
+++ main.go
@@ -1,32 +1,28 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
-// Both "fmt" and "net" are part of the Go standard library
 import (
-	// "fmt" has methods for formatted I/O operations (like printing to the console)
 	"fmt"
-	// The "net/http" library has methods to implement HTTP clients and servers
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	// The "HandleFunc" method accepts a path and a function as arguments
-	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
-	http.HandleFunc("/", handler)
-
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
-	http.ListenAndServe(":8080", nil)
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	return r
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
+func main() {
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	// For this case, we will always pipe "Hello World" into the response writer
 	fmt.Fprintf(w, "Hello World!")
 }
