ecd18acaae478978ef2a2d0beea8280e4a725d13
Krzysztof Kubicki <krzysztof.kubicki@fancyfon.com>
2018-02-14 20:33:27 +0100

    Serving properly parsed html with css and js, and passing a value of 42 to the parsed html - a dynamically served html

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 	"html/template"
 	"github.com/gorilla/mux"
+	"database/sql"
+
 )
 
 // The new router function creates the router and
 // returns it to us. We can now use this function
 // to instantiate and test the router outside of the main function
+
+var tpl *template.Template 
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./assets/*.html"))
+	
+}
+
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
 	r.HandleFunc("/morris", morris).Methods("GET")
+	r.HandleFunc("/db", database).Methods("GET")
 
 	staticFileDirectory := http.Dir("./assets/")
 
@@ -29,12 +40,24 @@ func main() {
 	http.ListenAndServe(":8080", r)
 }
 
+func database(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:root@/mysql")
+if err != nil {
+    panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+}
+defer db.Close()
+
+// Open doesn't open a connection. Validate DSN data:
+err = db.Ping()
+if err != nil {
+    panic(err.Error()) // proper error handling instead of panic in your app
+}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
 func morris(w http.ResponseWriter, r *http.Request) {
-	index := template.Must(template.ParseFiles(
-		"./assets/morris.html"))
-	index.Execute(w, nil)
+	tpl.ExecuteTemplate(w, "morris.html", 42)
 }
